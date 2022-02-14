d0bac3d7171711237e1cb507ced78822a06bf2c7
babuece97 <babuece97@yahoo.com.com>
2022-02-14 12:49:45 +0100

    anothe page

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
- const  prime=document.getElementById('prime');
- prime.addEventListener('click',function(){
-     console.log('clicked koreche');
+ const  primee=document.getElementById('prime');
+ primee.addEventListener('click',function(){
+     window.location.href="buy.html";
 
  });
-   
