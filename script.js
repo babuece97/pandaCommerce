e3e649306c51d6bcc71e389d10664f2014b478d3
babuece97 <babuece97@yahoo.com.com>
2022-02-14 10:50:55 +0100

    without slider

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
  const allBuyButtons = document.getElementsByClassName("buy-now-button");
-    console.log("buy-now-button");
+    console.log(allBuyButtons);
+
+    for (const button of allButtons) {
+        
+            console.log(button);
+
+        };
+     
