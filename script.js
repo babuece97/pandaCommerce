37856153243f6798b311810ba64dbf94d68ca777
babuece97 <babuece97@yahoo.com.com>
2022-02-14 14:43:08 +0100

    comment

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 
+ // for New page
  const  primee=document.getElementById('prime');
  primee.addEventListener('click',function(){
      window.location.href="buy.html";
 
  });
+
+ // for review section
+document.getElementById('comment').addEventListener('click', function(){
+    const commentBox=document.getElementById('feedback');
+    const userComment=commentBox.value;
+ 
+    const newComment=document.createElement('p');
+    newComment.innerText=userComment;
+
+    const commentContainer=document.getElementById('comment-container');
+    commentContainer.appendChild(newComment);
+})
